570f21cf8ed7bd9e081f341402e70a45f51f3900
iveltondequeirozm <iveltondequeiroz@gmail.com>
2019-01-22 16:42:48 -0400

    todo component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
 import logo from './block-bird-p.png';
 import './App.css';
+import Todos from './components/Todos';
 
 class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Context',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Http',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Charts',
+        completed: true
+      },
+      {
+        id: 4,
+        title: 'ToDo',
+        completed: true
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logos" alt="logo" />
           <p>
-            Block Bird
+            Block Birdthis.props.
           </p>
         </header>
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
+
+
+  markComplete = (e) => {
+    console.log('ok');
+  }
+
 }
 
 export default App;
